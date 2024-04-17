540121b2ea7649e5db30b471438340989b946e1c
unknown <sp577152@gmail.com>
2024-04-17 22:45:12 +0530

    Done

--- Backend/Controllers/UserController.js
+++ Backend/Controllers/UserController.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const getAllUsers = async (req, res) => {
   try {
     const Users = await UserModel.find({});
-    res.status(200).json({Users})
+    res.status(200).json(Users)
   } catch (error) {
     res.status(500).json({ message: "Unable to Fetch Data" });
   }
